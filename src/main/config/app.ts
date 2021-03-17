a2950de9885a4fd240ddd58bca0159f0d068aea6
vitoraa <vitor.lima@outlook.com>
2021-03-17 20:20:02 -0300

    feat: add signup routes returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
+import setuRoutes from './route'
 
 const app = express()
 setupMiddlewares(app)
+setuRoutes(app)
 export default app
