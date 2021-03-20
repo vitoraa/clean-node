0a7b0f6eb6ee85fdcf8c6882182f160c9f886ac2
vitoraa <vitor.lima@outlook.com>
2021-03-20 17:02:28 -0300

    test: ensure ValidationComposite returns the first error

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,46 @@
+import { InvalidParamError } from '../../errors'
+import { Validation } from './validation'
+import { ValidationComposite } from './validation-composite'
+
+interface SutTypes {
+  sut: Validation
+  validationStubs: Validation[]
+}
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+const makeSut = (): SutTypes => {
+  const validationStubs = [makeValidation(), makeValidation()]
+  const sut = new ValidationComposite(validationStubs)
+  return { sut, validationStubs }
+}
+
+describe('Validation Composite', () => {
+  test('Should return an Error if any validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new InvalidParamError('anyToCompare'))
+    const error = sut.validate({ any: 'any' })
+    expect(error).toEqual(new InvalidParamError('anyToCompare'))
+  })
+
+  test('Should return the first error if more than one validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error())
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new InvalidParamError('anyToCompare'))
+    const error = sut.validate({ any: 'any' })
+    expect(error).toEqual(new Error())
+  })
+
+  test('Should return nothing if all validations succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ any: 'any' })
+    expect(error).toBeFalsy()
+  })
+})
