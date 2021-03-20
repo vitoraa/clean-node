f2547c3569d6f1e0dc85cb9b0888465afa799a15
vitoraa <vitor.lima@outlook.com>
2021-03-20 09:44:48 -0300

    refactor: create separate file to LoginController

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,32 +1,7 @@
 import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
-import { Controller, HttpRequest, HttpResponse } from '../../protocols'
-import { EmailValidator } from '../signup/signup-protocols'
-
-class LoginController implements Controller {
-  private readonly emailValidator
-
-  constructor (emailValidator: EmailValidator) {
-    this.emailValidator = emailValidator
-  }
-
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['email', 'password']
-
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
-      }
-    }
-
-    if (!this.emailValidator.isValid(httpRequest.body.email)) {
-      return badRequest(new InvalidParamError('email'))
-    }
-
-    return await new Promise(resolve => resolve(null))
-  }
-}
+import { LoginController } from './login'
 
 interface SutTypes {
   sut: LoginController
---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,28 @@
+import { InvalidParamError, MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { EmailValidator } from '../signup/signup-protocols'
+
+export class LoginController implements Controller {
+  private readonly emailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = ['email', 'password']
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
+    }
+
+    if (!this.emailValidator.isValid(httpRequest.body.email)) {
+      return badRequest(new InvalidParamError('email'))
+    }
+
+    return await new Promise(resolve => resolve(null))
+  }
+}
