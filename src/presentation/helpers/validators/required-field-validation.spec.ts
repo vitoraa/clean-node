a2a36d1eee3576fdbe9b1c354f24a791b2cfcbb6
vitoraa <vitor.lima@outlook.com>
2021-03-20 16:29:22 -0300

    test: ensure RequiredFieldValidation returns nothing if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('Required Field Validation', () => {
     const error = sut.validate({ otherField: 'any' })
     expect(error).toEqual(new MissingParamError('field'))
   })
+
+  test('Should return nothing if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'any' })
+    expect(error).toBeFalsy()
+  })
 })
