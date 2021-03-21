cbc657ffea5a63437d41df9b08ee851becc927b3
vitoraa <vitor.lima@outlook.com>
2021-03-21 11:13:33 -0300

    feat: ensure returns null if account is not found

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -56,4 +56,11 @@ describe('DbAuthentication', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return null if no account is found', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(null)
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBeFalsy()
+  })
 })
