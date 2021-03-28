5121006d2789ce806a1ad8792dc9eed73e78fbe9
vitoraa <vitor.lima@outlook.com>
2021-03-28 09:49:05 -0300

    refactor: add auth middlewares to a separeted folder

--- src/main/routes/ship-routes.ts
+++ src/main/routes/ship-routes.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express'
-import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeAddShipController } from '../factories/controllers/ship/add-ship/add-ship-controller-factory'
 import { makeLoadShipsController } from '../factories/controllers/ship/load-ships/load-ships-controller-factory'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
+import { adminAuth } from '../middlewares/admin-auth'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
-  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
-  const auth = adaptMiddleware(makeAuthMiddleware())
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/ships', adminAuth, adaptRoute(makeAddShipController()))
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
