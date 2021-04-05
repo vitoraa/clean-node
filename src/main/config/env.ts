f1e0a2fdcdea3df2c9c589f63f37a728813d4284
vitoraa <vitor.lima@outlook.com>
2021-04-05 09:02:34 -0300

    chore: travis config to use postgres

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,5 @@ export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H',
-  pgUrl: process.env.PG_URL || 'postgresql://postgres:postgres@localhost:5432/clean-node-api'
+  pgUrl: process.env.PG_URL || 'postgresql://postgres:postgres@localhost:5432/cleannodeapi'
 }
